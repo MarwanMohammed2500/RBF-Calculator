e4f180fd2ad4b4cd3d25bc81d1c9c5d1e4f4149e
Marwan Mohammed Sayed <155132957+MarwanMohammed2500@users.noreply.github.com>
2025-02-26 08:51:56 +0200

    Update RBF.py

--- RBF.py
+++ RBF.py
@@ -4,11 +4,29 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 class RBF:
-    def __init__(self):
-        pass
-        
-    
+    """
+    This class uses a sequence of functions to calculate the Radial Basis Function (AKA RBF)
+    get_input():
+        Used to get the user input from the UI, input like centroid locations, features (X1 and X2), category of the sample, and the variance.
+    calc_r():
+        Used to calculate the squared distance between the points and each centroid.
+    calc_phi():
+        Used to calculate the transformed features ∅1 and ∅2.
+    plot_x():
+        Used to plot the original feature space.
+    plot_phi():
+        Used to plot the transformed feature space.
+    """
     def get_input(self):
+        """
+        This function is responsible for getting user input from the interface and making sure the user inputs the right data types into their respective fields.
+        Inputs:
+        self.df --> the DataFrame representing our table, from which we will read, and to which we will write
+        self.c1 --> The first centroid coordinates
+        self.c2 --> The second centroid coordinates
+        self.segma_sq --> The variance
+        """
+
         __data = st.data_editor(pd.DataFrame({'category': [], 'x1': [], "x2":[]}), num_rows="dynamic")
         self.df = pd.DataFrame(__data)
         self.c1 = st.text_input("Enter c1 (split values with a comma ','):")
@@ -25,25 +43,43 @@ class RBF:
             st.info("Please provide input data to proceed.")
     
     def calc_r(self):
+        """
+        This function calculates the distance between each point and each centroid, where R1 represents the distances between the points and the first centroid, and R2 represents the distances
+        between the points and the second centroid.
+
+        r1_sq --> Squared distance between the points and C1
+        r2_sq --> Squared distance between the points and C2
+        """
+
         r1_sq= (self.df.x1 - self.c1[0])**2 + (self.df.x2 - self.c1[1])**2
         r2_sq= (self.df.x1 - self.c2[0])**2 + (self.df.x2 - self.c2[1])**2
-        self.df = self.df.join(pd.DataFrame({'r1 ^2': r1_sq, "r2 ^2": r2_sq}, index=self.df.index))
-        st.session_state.df = self.df        
+        self.df = self.df.join(pd.DataFrame({'r1 ^2': r1_sq, "r2 ^2": r2_sq}, index=self.df.index)) # Joins the new calculated columns with the original DataFrame
+        st.session_state.df = self.df # Save the DataFrame in the session state so that it can be updated and read in the next step
     
     def calc_phi(self):
-        self.df = st.session_state.get("df", self.df)
+        """
+        This function calculates the transformed features, ∅1 and ∅2, via the equation:
+        ∅1 = (e^(-r1^2/2*𝝈^2))
+        ∅2 = (e^(-r2^2/2*𝝈^2))
+        """
+        
+        self.df = st.session_state.get("df", self.df) # Reads the session state DataFrame, so it gets the updated value after calculating R1^2 and R2^2
         phi1= np.exp(-self.df["r1 ^2"] / (2 * self.segma_sq))
         phi2= np.exp(-self.df["r2 ^2"] / (2 * self.segma_sq))
-        self.df = self.df.join(pd.DataFrame({'∅1': phi1, "∅2": phi2}, index=self.df.index))
+        self.df = self.df.join(pd.DataFrame({'∅1': phi1, "∅2": phi2}, index=self.df.index)) 
         st.session_state.df = self.df
     
     def plot_x(self):
+        """
+        This function plots the original feature space
+        """
+        
         fig, ax = plt.subplots()
         self.df = st.session_state.get("df", self.df)
-        light_df = self.df[self.df["category"] == 1]
+        light_df = self.df[self.df["category"] == 1] # Light Category
         ax.scatter(light_df.x1, light_df.x2, linewidth=10,color='aquamarine', label="Light")
 
-        dark_df = self.df[self.df["category"] == 0]
+        dark_df = self.df[self.df["category"] == 0] # Dark Category
         ax.scatter(dark_df.x1, dark_df.x2, linewidth=10, color='darkred', label="Dark")
 
         plt.style.use('ggplot')
@@ -55,6 +91,10 @@ class RBF:
         st.pyplot(fig)
     
     def plot_phi(self):
+        """
+        This function plots the transformed feature space
+        """
+        
         fig, ax = plt.subplots()
         self.df = st.session_state.get("df", self.df)
         light_df = self.df[self.df["category"] == 1]
@@ -71,6 +111,10 @@ class RBF:
         st.pyplot(fig)
     
     def show_table(self):
+        """
+        This function shows the table (DataFrame) in the streamlit UI, it can be used any time throughout execution (edit in main.py), but for now, main.py is made to use this only after
+        executing calc_r and calc_phi
+        """
         self.df = st.session_state.get("df", self.df)
-        st.write("Full Table:")
+        st.write("Original + Transformation Table:")
         st.dataframe(self.df)
